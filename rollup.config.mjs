3646b42eb5f4aa49083deddddfe0beccdaf61ea0
Borgar <borgar@borgar.net>
2025-05-23 11:34:53 +0000

    Break files apart to fix circular deps
    Also added a few more unit tests & updated rollup minifier plugin

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,8 +1,8 @@
 import fs from 'fs';
 import babel from '@rollup/plugin-babel';
-import { terser } from 'rollup-plugin-minification';
+import terser from '@rollup/plugin-terser';
 
-const pkg = JSON.parse(fs.readFileSync('./package.json'));
+const pkg = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
 const isProd = process.env.NODE_ENV === 'production';
 
 export default {
@@ -11,12 +11,12 @@ export default {
     babel({
       babelHelpers: 'bundled',
       presets: [ '@babel/preset-env' ]
-    })
+    }),
+    isProd ? terser({ maxWorkers: 4 }) : null
   ],
   output: {
     file: pkg.main,
     format: 'cjs',
-    sourcemap: 'inline',
-    plugins: [ isProd && terser() ]
+    sourcemap: 'inline'
   }
 };
