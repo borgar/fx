0c28d5d601a1fd11e5c2adf81e603cfc5475a8dd
Borgar <borgar@borgar.net>
2025-10-27 11:01:49 +0000

    Add A1 parsing benchmark
    Also, stop publishing the benchmark data with the package

--- benchmark/benchmark.js
+++ benchmark/benchmark.js
@@ -3,19 +3,31 @@ import { readFileSync } from 'node:fs';
 import benchmark from 'benchmark';
 import { tokenize as tokenizeCurr } from '../lib/lexer.js';
 import { parse as parseCurr } from '../lib/parser.js';
-import { tokenize as tokenizePrev, parse as parsePrev } from '../dist/fx.js';
+import { parseA1Ref as parseA1RefCurr } from '../lib/a1.js';
+import {
+  tokenize as tokenizePrev,
+  parse as parsePrev,
+  parseA1Ref as parseA1RefPrev
+} from '../dist/fx.js';
 
 const formulas = JSON.parse(readFileSync('benchmark/formulas.json', 'utf8'));
+const refs = JSON.parse(readFileSync('benchmark/refs.json', 'utf8'));
 
-function runSuite (title, current, previous) {
+const opts = { allowTernary: true };
+const filter = (process.argv[2] ?? '').toLowerCase();
+
+function runSuite (title, current, previous, testSet) {
+  if (filter && !title.toLowerCase().includes(filter)) {
+    return;
+  }
   return new Promise(resolve => {
     console.log('Running benchmark for ' + title);
     // @ts-ignore
     (new benchmark.Benchmark.Suite())
       .add('lib/' + title, () => {
-        for (const expr of formulas) {
+        for (const expr of testSet) {
           try {
-            current(expr);
+            current(expr, opts);
           }
           catch (err) {
             // gulp
@@ -23,9 +35,9 @@ function runSuite (title, current, previous) {
         }
       })
       .add('dist/' + title, () => {
-        for (const expr of formulas) {
+        for (const expr of testSet) {
           try {
-            previous(expr);
+            previous(expr, opts);
           }
           catch (err) {
             // gulp
@@ -44,5 +56,6 @@ function runSuite (title, current, previous) {
   });
 }
 
-await runSuite('tokenizer', tokenizeCurr, tokenizePrev);
-await runSuite('parser', parseCurr, parsePrev);
+await runSuite('tokenizer', tokenizeCurr, tokenizePrev, formulas);
+await runSuite('parser', parseCurr, parsePrev, formulas);
+await runSuite('parseA1Ref', parseA1RefCurr, parseA1RefPrev, refs);
